225cef3ff4140d596e485ff4341bc090f3dece54
Linus Ilbratt <linusilbratt@gmail.com>
2024-09-06 08:25:57 +0200

    changed comments

--- api/server.js
+++ api/server.js
@@ -1,23 +1,22 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');  // axios for easier API calls
-const app = express();
+require('dotenv').config(); // load environment variable from .env
+const express = require('express'); // express is a framework to build webb apps in Node.js
+const axios = require('axios');  // axios library for easier API calls
+const app = express(); // create a express-app to handle HTTP requests
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY; // get API key from the .env file
 
-const path = require('path');
+const path = require('path'); // Node module for file-catalouge searches
 
-// Serve static files (HTML, CSS, JS) from the public folder
+// serve static files (HTML, CSS, JS) from the public folder
 app.use(express.static(path.join(__dirname, '../public')));
 
 // define a route to handle the API request
-console.log('API Key:', API_KEY);  // Add this for debugging
 app.get('/api/news', async (req, res) => {
   const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`;
   
   try {
     const response = await axios.get(url);
-    res.json(response.data);  // send the data to the frontend
+    res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching data from the API' });
   }
