8165b650e4e6d373a97c3467994d3756b0bd6f8f
Linus Ilbratt <linusilbratt@gmail.com>
2024-09-06 08:56:14 +0200

    Combining API requests dynamic with a loop with the use of Promise.all

--- api/server.js
+++ api/server.js
@@ -12,11 +12,26 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // define a route to handle the API request
 app.get('/api/news', async (req, res) => {
-  const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`;
+  const url = [
+    `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`
+    `https://newsapi.org/v2/everything?q=apple&from=2024-09-05&to=2024-09-05&sortBy=popularity&apiKey=${API_KEY}`
+  ];
+  
   
   try {
-    const response = await axios.get(url);
-    res.json(response.data);
+    // loop through the API:s and sends all requests pararell
+    const apiRequests = apiUrls.map(url => axios.get(url));
+    const response = await Promise.all(apiRequests);
+
+    // extract every article from each API answer
+    const articles = response.map(response => response.data.articles);
+
+    // return the results
+    res.json({
+      techCrunchArticles: articles[0],
+      appleArticles: articles[1]
+    });
+
   } catch (error) {
     res.status(500).json({ error: 'Error fetching data from the API' });
   }
